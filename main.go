8fb0fafff14041a0eaac588fd57a5cc93bb5b7a5
bcschwimm <billcoll@protonmail.com>
2021-07-24 08:20:44 -0400

    todos added wip

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
+	"strconv"
 )
 
+// CurrentWeather matches the return of our open
+// weather api
 type CurrentWeather struct {
-	Condition struct {
+	Condition []struct {
 		Description string `json:"description"`
 	} `json:"weather"`
 
@@ -20,12 +27,61 @@ type CurrentWeather struct {
 
 	Sys struct {
 		Sunrise int `json:"sunrise"`
-		Sunset  int `json:"sunrise"`
+		Sunset  int `json:"sunset"`
 	}
 
 	Name string `json:"name"`
 }
 
+///// define print method for struct
+///// create & define api call method for api string custom type, have method return w.
+
+type apiUrl string
+
+// apiUrlString validates a user input and returns either the api
+// call url for a zip code search, or a city name search
+func apiUrlString(userInput string) apiUrl {
+	_, err := strconv.Atoi(userInput)
+
+	if err != nil {
+		return apiUrl(fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s", userInput, os.Getenv("WEATHER_KEY")))
+	}
+	return apiUrl(fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?zip=%s&appid=%s", userInput, os.Getenv("WEATHER_KEY")))
+}
+
+// apiCall takes in a search city name and returns
+// a populated CurrentWeather struct with weather data
+func apiCall(cityName string) CurrentWeather {
+
+	s := fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s", cityName, os.Getenv("WEATHER_KEY"))
+
+	resp, err := http.Get(s)
+
+	if err != nil {
+		fmt.Printf("Error with API Request %s\n", err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Printf("Error Reading Response Body %s\n", err)
+	}
+
+	var weatherData CurrentWeather
+	err = json.Unmarshal(body, &weatherData)
+	if err != nil {
+		fmt.Printf("Error unmarshalling json body into WeatherData struct %s\n", err)
+	}
+
+	return weatherData
+}
+
+// implement flag arg for city, with the default being our current location
 func main() {
-	fmt.Println("ENV:", os.Getenv("WEATHER_KEY"))
+
+	var citySearch = flag.String("c", "philadelphia, pa", "enter a city, state abbreviation to search the weather")
+	flag.Parse()
+	w := apiCall(*citySearch)
+
+	fmt.Println(w.Name)
 }
